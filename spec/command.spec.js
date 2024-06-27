e00499cc6abbd105928894277ef67126bb28f202
Alec <alecjost7@gmail.com>
2024-06-27 15:34:08 -0500

    Mars Rover Command Class Completed

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -8,5 +8,13 @@ describe("Command class", function() {
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
+  
+  test("constructor sets command type", function(){
+    expect(function() { new Command();});
+  })
+
+  test("constructor sets a value passed in as the 2nd argument", function(){
+    expect(function() { new Command().value});
+  })
 
 });
